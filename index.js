05f00cf99881eb480e27df6664a38d3d0036f0fe
Fernando Cristobal De La Cruz <fernandocgomez@live.com>
2019-08-21 05:08:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  var newObject = {key: 'value'}
+  var newObject = { key: 'value' }
   delete key.value;
   return newobject
 }
